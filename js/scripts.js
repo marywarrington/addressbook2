081ebbbe4f53f92ba0cd17f06bbbfac48e5b7cd5
mary warrington and paul hess <pauljacksonhess@gmail.com>
2016-01-25 09:50:28 -0800

    Add initial UI and index.html form

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,19 @@ function Contact(firstName, lastName, street, city, state) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
+$(document).ready(function() {
+  $('form#contact').submit(function(event) {
+    event.preventDefault();
+    var firstNameInput = $('input#firstName').val();
+    var lastNameInput = $('input#lastName').val();
+    var streetInput = $('input#street').val();
+    var cityInput = $('input#city').val();
+    var stateInput = $('input#state').val();
+
+    var newContact = new Contact(firstNameInput, lastNameInput, streetInput, cityInput, stateInput);
+
+    $("ul#nameList").append("<li>" + newContact.fullName() + "</li>");
+
+  });
+});
